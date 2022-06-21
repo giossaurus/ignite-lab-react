d795216caa952846765e3756cb7e6ec7f7044067
Giossaurus <gihhskt@gmail.com>
2022-06-21 18:23:34 -0300

    query com apollo

--- event-platform/src/App.tsx
+++ event-platform/src/App.tsx
@@ -1,26 +1,31 @@
-import { gql } from "@apollo/client"
+import { gql, useQuery } from "@apollo/client"
 import { useEffect } from "react"
-import { client } from "./lib/apollo"
+// import { client } from "./lib/apollo"
 
 const GET_LESSONS_QUERY = gql`
   query{
-    teachers{
-      name
+    lessons {
+      id
+      title
     }
   } 
 `
+interface Lesson {
+  id: string;
+  title: string;
+}
 
 function App() {
-  useEffect(() => {
-    client.query({
-      query: GET_LESSONS_QUERY,
-    }).then(response => {
-      console.log(response.data)
-    })
-  }, [])
+  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
+
+  console.log(data);
 
   return (
-    <h1 className="text-5xl font-bold text-violet-500">Hello World</h1>
+    <ul>
+      {data?.lessons.map(lesson =>{
+        return <li key={lesson.id}>{lesson.title}</li>
+      })}
+    </ul>
   )
 }
 
