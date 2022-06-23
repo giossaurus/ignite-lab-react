fdb484ad5bc0a37a20ed919968804e3fc550fb13
Giossaurus <gihhskt@gmail.com>
2022-06-22 23:47:16 -0300

    event

--- event-platform/src/App.tsx
+++ event-platform/src/App.tsx
@@ -1,7 +1,13 @@
+import { Header } from "./components/Header"
+import { Lesson } from "./components/Lesson"
+import { Sidebar } from "./components/Sidebar"
+import { Video } from "./components/Video"
+import { Event } from "./pages/Event"
+
 function App() {
   return (
     <div>
-      
+      <Event />
     </div>
   )
 }
