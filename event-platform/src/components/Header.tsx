6a28b6fca1ee927c778488e1b413b4e4fb2e49dc
Giossaurus <gihhskt@gmail.com>
2022-06-22 23:58:39 -0300

    Update Header.tsx

--- event-platform/src/components/Header.tsx
+++ event-platform/src/components/Header.tsx
@@ -1,7 +1,9 @@
-import Logo from "./Logo";
+import { Logo } from "./Logo";
 
 export function Header() {
     return (
-        <Logo />
+        <header className="w-full py-5 flex items-center justify-center bg-black">
+          <Logo />  
+        </header>
     )
 }
