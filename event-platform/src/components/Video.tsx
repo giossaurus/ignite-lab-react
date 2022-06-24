6adbba7f97afc5172cce954dc3c98977d600df05
Giossaurus <gihhskt@gmail.com>
2022-06-24 00:47:43 -0300

    lessons slug props added

--- event-platform/src/components/Video.tsx
+++ event-platform/src/components/Video.tsx
@@ -1,9 +1,48 @@
 import { DefaultUi, Player, Youtube } from "@vime/react";
 import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from "phosphor-react";
+import { gql, useQuery } from "@apollo/client";
 
 import '@vime/core/themes/default.css'
 
-export function Video() {
+
+const GET_LESSON_BY_SLUG_QUERY = gql `
+    query GetLessonBySlug ($slug: String) {
+        lesson(where: {slug: $slug}) {
+        title
+        videoId
+        description
+        teacher {
+         name
+         bio
+         avatarURL
+        }
+    }
+}
+`
+interface GetLessonBySlugResponse {
+    lesson: {
+        title: string;
+        videoId: string;
+        description: string;
+        teacher: {
+            bio: string;
+            avatarURL: string;
+            name: string;
+        }
+    }
+}
+
+interface VideoProps {
+    lessonSlug: string;
+}
+
+export function Video(props: VideoProps) {
+    const { data } = useQuery(GET_LESSON_BY_SLUG_QUERY, {
+        variables: {
+            slug: props.lessonSlug,
+        }
+    })
+
     return(
         <div className="flex-1">
             <div className="bg-black flex justify-center">
